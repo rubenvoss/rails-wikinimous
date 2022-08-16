8bcba0f0ee145925e67e3c6fe7a0fc43ebde7597
Ruben Voss <bekertshop@gmail.com>
2022-08-16 15:46:01 +0200

    added update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,15 +3,37 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    @article = Article.create(article_params)
+    redirect_to article_path(@article)
+  end
+
   def index
     @articles = Article.all
   end
 
-  def create
-    @article = Article.create(params[:article])
-  end
-
   def show
     @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = article.find(params[:id])
+    @article.update(article_params)
+  end
+
+  def destroy
+    article = article.find(params[:id])
+    article.destroy
+    redirect_to articles_path, status: :see_other
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
